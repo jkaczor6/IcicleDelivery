e64101ba045896194825105d05d0f0d22b38ec76
Jakub Kaczor <j.kaczor06@gmail.com>
2025-11-29 22:52:16 +0100

    added prefabs, created level, added package pickup and delivery system, created particle system

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage = false;
+    [SerializeField] float destroyDelay = 0.3f;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Package") && !hasPackage)
+        {
+            Destroy(collision.gameObject, destroyDelay);
+            hasPackage = true;
+            GetComponent<ParticleSystem>().Play();
+        }
+        else if(collision.CompareTag("Customer") && hasPackage)
+        {
+            hasPackage = false;
+            GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
